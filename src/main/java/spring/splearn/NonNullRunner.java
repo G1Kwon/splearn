e840bb6f931eb92a63e1dc26d8b29bb2c1c39922
g1kwon <spring_shot@naver.com>
2026-01-23 23:37:02 +0900

    Null 안정성 확보

---
+++ src/main/java/spring/splearn/NonNullRunner.java
@@ -0,0 +1,16 @@
+package spring.splearn;
+
+import org.springframework.lang.NonNull;
+
+public class NonNullRunner {
+
+  public static void main(String[] args) {
+    String name = null;
+    print(name);
+  }
+
+  static void print(@NonNull String name) {
+    System.out.println(name);
+  }
+
+}
