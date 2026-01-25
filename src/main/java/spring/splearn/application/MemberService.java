ccc38785ae0b9bd9218da46d682d6408b4e86586
g1kwon <spring_shot@naver.com>
2026-01-25 23:58:39 +0900

    회원 애플리케이션 서비스 구현

---
+++ src/main/java/spring/splearn/application/MemberService.java
@@ -0,0 +1,34 @@
+package spring.splearn.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.splearn.application.provided.MemberRegister;
+import spring.splearn.application.required.EmailSender;
+import spring.splearn.application.required.MemberRepository;
+import spring.splearn.domain.Member;
+import spring.splearn.domain.MemberRegisterRequest;
+import spring.splearn.domain.PasswordEncoder;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements MemberRegister {
+
+  private final MemberRepository memberRepository;
+  private final EmailSender emailSender;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public Member register(MemberRegisterRequest registerRequest) {
+    // check
+
+    // domain model
+    Member member = Member.register(registerRequest, passwordEncoder);
+
+    //repository
+    memberRepository.save(member);
+
+    //post process
+    emailSender.send(member.getEmail(), "등록을 완료해주세요", "아래 링크를 클릭해서 등록을 완료해주세요");
+    return null;
+  }
+}
